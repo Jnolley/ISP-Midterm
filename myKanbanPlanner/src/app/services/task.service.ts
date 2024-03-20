828a4c5c46b1483030fbc4129da483e58f9d79b7
Jnolley <jackmnolley@gmail.com>
2024-03-19 22:25:23 -0500

    added storage to local storage so that it doesnt lose your stuff after every time you reload

--- myKanbanPlanner/src/app/services/task.service.ts
+++ myKanbanPlanner/src/app/services/task.service.ts
@@ -3,15 +3,23 @@ import { BehaviorSubject } from 'rxjs';
 import { Task } from '../models/task.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class TaskService {
   private tasksSubject = new BehaviorSubject<Task[]>([]);
   public tasks$ = this.tasksSubject.asObservable();
-  
 
-  constructor() { }
+  constructor() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasksSubject.next(JSON.parse(storedTasks));
+    }
+  }
+
+  private saveTasksToLocalStorage(): void {
+    localStorage.setItem('tasks', JSON.stringify(this.tasksSubject.getValue()));
+  }
 
   addTask(task: Task): void {
     const currentTasks = this.tasksSubject.getValue();
@@ -28,18 +36,18 @@ export class TaskService {
     this.tasksSubject.next([...currentTasks, newTask]);
     console.log('Task added:', newTask);
     console.log('All tasks:', this.tasksSubject.getValue());
+    this.saveTasksToLocalStorage();
   }
 
   updateTaskStatus(taskId: number, newStatus: string): void {
     const tasks = this.tasksSubject.getValue();
-    const taskIndex = tasks.findIndex(task => task.id === taskId);
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
 
     if (taskIndex !== -1) {
       tasks[taskIndex].status = newStatus as Task['status'];
-      
+
       this.tasksSubject.next(tasks);
     }
+    this.saveTasksToLocalStorage();
   }
-
-  
 }
